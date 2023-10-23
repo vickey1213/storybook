38e094a35ac834b9e0787d6802dd8b92770c9390
Sohini Bhattacharjee <sohinismail@gmail.com>
2023-10-23 18:08:44 +0530

    Changed some styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Body from "@/components/ui/Body";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-[#0e1117] font-mono ">
-      <div className="border-[0.15px] border-gray-700 max-w-[736px] my-20">
+    <main className="flex flex-col items-center bg-[#0e1117] font-mono px-4">
+      <div className="max-w-screen-md w-full border-[0.15px] border-gray-700 my-20">
         <Header />
         <Body />
         <Parameter />
