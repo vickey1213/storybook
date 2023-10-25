2ca2958fa9a56b1f889a92bb4be1e0e01da765a7
Sohini Bhattacharjee <sohinismail@gmail.com>
2023-10-25 15:34:32 +0530

    Added responsive styling

--- components/ui/Parameter/Parameter.tsx
+++ components/ui/Parameter/Parameter.tsx
@@ -8,13 +8,13 @@ const Parameter = () => (
     {parameterData.map((parameter, index) => (
       <Card
         key={index}
-        className="flex bg-[#171a21] shadow-xl text-white justify-items-start items-center border-b-[0.15px] border-gray-700"
+        className="flex bg-[#171a21] shadow-xl text-white text-sm justify-items-start items-center border-b-[0.15px] border-gray-700"
       >
-        <CardContent className="flex gap-2 w-[30%]">
-          <h3>{parameter.title}</h3>
-          <h3 className="text-green-500">{parameter.value}</h3>
+        <CardContent className="flex gap-2 w-[30%] overflow-auto">
+          {parameter.title}
+          <p className="text-green-500">{parameter.value}</p>
         </CardContent>
-        <CardContent className="w-[70%] flex items-start">
+        <CardContent className="w-[70%] flex">
           {parameter.description}
         </CardContent>
       </Card>
